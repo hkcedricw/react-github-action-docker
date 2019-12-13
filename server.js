aead2fdde61ead3c3cafb96b9e39caf4786b2e08
Cedric Wong <hkcedricw@gmail.com>
2019-12-13 17:05:01 +0800

    create react app and express to run build

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const PORT  = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`web app listening on port ${PORT}`);
+});
